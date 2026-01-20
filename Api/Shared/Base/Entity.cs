57c67a21fc49e38f26d51bad0d90e868ff680c10
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2026-01-20 15:29:43 -0300

    feat: adiciona a propriedade de nome como padrão.

--- Api/Shared/Base/Entity.cs
+++ Api/Shared/Base/Entity.cs
@@ -3,19 +3,22 @@ namespace Api.Shared.Base;
 public abstract class Entity
 {
     public Guid Id { get; protected set; }
+    public string Name { get; protected set; }
     public DateTime Created { get; protected set; }
     public bool Active { get; protected set; }
 
-    public Entity()
+    public Entity(string name)
     {
         Id = Guid.NewGuid();
+        Name = name;
         Created = DateTime.UtcNow;
         Active = true;
     }
 
-    public Entity(Guid id, DateTime created, bool active)
+    public Entity(string name, Guid id, DateTime created, bool active)
     {
         Id = id;
+        Name = name;
         Created = created;
         Active = active;
     }
