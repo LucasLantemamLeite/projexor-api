7ebae66dc4fc3fcbc426f2a6736487373cb59de1
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2026-01-20 15:40:56 -0300

    feat: cria a classe que representa os usuários

---
+++ Api/Features/Users/Models/User.cs
@@ -0,0 +1,19 @@
+using Api.Shared.Base;
+
+namespace Api.Features.Users.Models;
+
+public sealed class User : Entity
+{
+    public string Email { get; private set; }
+    public string Phone { get; private set; }
+    public string Password { get; private set; }
+
+    // Usado para instanciar novos usuários
+    public User(string name) : base(name) { }
+
+    // Usado para instanciar usuários salvos no banco
+    public User(Guid id, string name, DateTime created) : base(id, name, created)
+    {
+
+    }
+}
