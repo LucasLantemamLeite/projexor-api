baf42d300e87eadde699c24603fd81a5ae4bbeb5
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2026-01-21 15:36:38 -0300

    feat: adiciona a propriedade de nome removida e adiciona relacionamento

--- Api/Features/Projects/Models/Project.cs
+++ Api/Features/Projects/Models/Project.cs
@@ -1,18 +1,24 @@
+using Api.Features.Groups.Models;
 using Api.Features.Projects.Enums;
+using Api.Features.Users.Models;
 using Api.Shared.Base;
 
 namespace Api.Features.Projects.Models;
 
 public sealed class Project : Entity
 {
+    public string Name { get; private set; }
     public EStatus Status { get; private set; }
     public DateTime? Deadline { get; private set; }
     public Guid OwnerId { get; private set; }
     public bool IsPersonal { get; private set; }
+    public User? User { get; private set; }
+    public Group? Group { get; private set; }
 
     // Usado para instanciar novos Projetos
-    public Project(string name, EStatus status, DateTime? deadline, Guid ownerId, bool isPersonal) : base(name)
+    public Project(string name, EStatus status, DateTime? deadline, Guid ownerId, bool isPersonal)
     {
+        Name = name;
         Status = status;
         Deadline = deadline;
         OwnerId = ownerId;
@@ -20,8 +26,9 @@ public sealed class Project : Entity
     }
 
     // Usado para instanciar Projetos salvos no banco
-    public Project(Guid id, string name, DateTime created, EStatus status, DateTime? deadline, Guid ownerId, bool isPersonal) : base(id, name, created)
+    public Project(Guid id, string name, DateTime created, EStatus status, DateTime? deadline, Guid ownerId, bool isPersonal) : base(id, created)
     {
+        Name = name;
         Status = status;
         Deadline = deadline;
         OwnerId = ownerId;
--- Api/Features/Users/Models/User.cs
+++ Api/Features/Users/Models/User.cs
@@ -1,24 +1,29 @@
+using Api.Features.Projects.Models;
 using Api.Shared.Base;
 
 namespace Api.Features.Users.Models;
 
 public sealed class User : Entity
 {
+    public string Name { get; private set; }
     public string Email { get; private set; }
     public string Phone { get; private set; }
     public string Password { get; private set; }
+    public ICollection<Project> Projects { get; private set; } = [];
 
     // Usado para instanciar novos usuários
-    public User(string name, string email, string phone, string password) : base(name)
+    public User(string name, string email, string phone, string password)
     {
+        Name = name;
         Email = email;
         Phone = phone;
         Password = password;
     }
 
     // Usado para instanciar usuários salvos no banco
-    public User(Guid id, string name, DateTime created, string email, string phone, string password) : base(id, name, created)
+    public User(Guid id, string name, DateTime created, string email, string phone, string password) : base(id, created)
     {
+        Name = name;
         Email = email;
         Phone = phone;
         Password = password;
